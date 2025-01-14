a7e7da9270451ebf6b8971979281efe8f9bfcc4d
MD. NIZAM UDDIN TUHIN <112401451+nizamuddintuhin@users.noreply.github.com>
2025-01-14 18:30:25 +0600

    Update environment.ts

--- HrmsAngular/src/environments/environment.ts
+++ HrmsAngular/src/environments/environment.ts
@@ -6,14 +6,5 @@
 export const environment = {
   production: false,
   apiUrl:'http://localhost:9113/api/v1',
-   //apiUrl: 'http://103.118.19.110:1401/api/v1',
-//  reportApiUrl : 'http://localhost:5200/api/v1/report',
-  // apiUrl:'http://103.108.140.183:1021/api/v1',
-  //  apiUrl:'http://103.108.140.183:1002/api/v1',
-
- // reportApiUrl : 'http://192.168.4.153:1403/api/v1/report'
-
-
-
-  reportApiUrl : 'http://103.118.19.110:1403/api/v1/report'
+  
 };
